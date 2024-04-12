0d1b05af1a52e523c84da5835cb9ec163d365c01
Tae Oey <tae.youn.kim.oey@accenturefederal.com>
2024-04-12 18:31:11 -0400

    Add USWDS Subtheme

---
+++ web/themes/custom/uswds_subtheme/gulpfile.js
@@ -0,0 +1,26 @@
+/* gulpfile.js */
+
+// eslint-disable-next-line import/no-unresolved
+const uswds = require('@uswds/compile');
+
+/**
+ * USWDS version
+ */
+
+uswds.settings.version = 3;
+
+/**
+ * Path settings
+ * Set as many as you need
+ */
+
+uswds.paths.dist.css = './css';
+uswds.paths.dist.theme = './sass';
+
+/**
+ * Exports
+ * Add as many as you need
+ */
+
+exports.init = uswds.init;
+exports.compile = uswds.compile;
